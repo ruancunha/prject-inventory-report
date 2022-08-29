8b813e8950eb6b00c43399ec96ae4562a01e5605
RuanCunha <ruanvcunha@hotmail.com>
2022-08-29 15:33:08 -0300

    test: 💍 create product_report test

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,22 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    'Testa o repr da classe Product'
+    produto_teste = Product(
+        id=1,
+        nome_do_produto="Produto Teste",
+        nome_da_empresa="Empresa 1",
+        data_de_fabricacao="10/10/2020",
+        data_de_validade="11/11/2020",
+        numero_de_serie=123456,
+        instrucoes_de_armazenamento="em local fresco")
+
+    assert repr(produto_teste) == (
+            f"O produto {produto_teste.nome_do_produto}"
+            f" fabricado em {produto_teste.data_de_fabricacao}"
+            f" por {produto_teste.nome_da_empresa} com validade"
+            f" até {produto_teste.data_de_validade}"
+            f" precisa ser armazenado "
+            f"{produto_teste.instrucoes_de_armazenamento}."
+        )
