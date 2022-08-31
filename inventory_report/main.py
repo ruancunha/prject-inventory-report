a4e9828dfbef0ec987cda1be6fb0af9438c50d4c
RuanCunha <ruanvcunha@hotmail.com>
2022-08-30 21:48:54 -0300

    feat: 🎸 implement main.py
    BREAKING CHANGE: 🧨 pip not installing, not running on shell; conflict with test output;

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,26 @@
+import sys
+from inventory_report.importer.csv_importer import CsvImporter
+
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+from .inventory.inventory_refactor import InventoryRefactor
+
+
 def main():
-    pass
+    if len(sys.argv) != 3:
+        print("Verifique os argumentos")
+    else:
+        file = sys.argv[1]
+        type = sys.argv[2]
+        if file.endswith(".json"):
+            inventory = InventoryRefactor(JsonImporter)
+        elif file.endswith(".csv"):
+            inventory = InventoryRefactor(CsvImporter)
+        else:
+            inventory = InventoryRefactor(XmlImporter)
+
+        print(inventory.import_data(file, type))
+
+
+if __name__ == "__main__":
+    main()
